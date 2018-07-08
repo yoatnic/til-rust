7724f8deaa5591fa816c6377dbde207f7ab5ebed
yoat <yoatnic@me.com>
2018-07-08 19:13:57 +0900

    to module

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod random_number;
+
+fn main() {
+    random_number::random_exapmle();
+}
---
+++ src/random_number.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+extern crate std;
+
+use self::std::io;
+use self::rand::Rng;
+
+pub fn random_exapmle() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
