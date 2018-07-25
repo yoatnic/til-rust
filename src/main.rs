c2be59a4617cd5a0dea22852b890471baac9c006
yoat <yoatnic@me.com>
2018-07-25 20:52:37 +0900

    enunm

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@
 // mod guess;
 // mod expression;
 // mod owner_ship;
-
-mod phrases;
+mod enum_example;
+// mod phrases;
 
 fn main() {
-    println!("Hello in English: {}", phrases::english::greetings::hello());
-    println!("Goodbye in English: {}", phrases::english::farewells::goodbye());
+    // println!("Hello in English: {}", phrases::english::greetings::hello());
+    // println!("Goodbye in English: {}", phrases::english::farewells::goodbye());
 
-    println!("Hello in Japanese: {}", phrases::japanese::greetings::hello());
-    println!("Goodbye in Japanese: {}", phrases::japanese::farewells::goodbye());
+    // println!("Hello in Japanese: {}", phrases::japanese::greetings::hello());
+    // println!("Goodbye in Japanese: {}", phrases::japanese::farewells::goodbye());
 
     // owner_ship::owner_ship();
     // expression::expression();
     // hello_world::hello_world();
     // guess::guess();
+    enum_example::enum_example();
 }
